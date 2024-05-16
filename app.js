6ec1ff54a837c9cee3ee03929b9151261cb11e58
veyhn <109932151+veyhn@users.noreply.github.com>
2024-05-15 21:04:00 -0400

    setting up app.js and router. will work on authentication for user login and secure registration

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app= express();
+const PORT = 3000;
+const authRouter = require('./AuthController/authRouter');
+
+// receive json requests properly
+app.use(express.json());
+
+app.get('/', function(req,res){
+    res.json({message: 'Welcome to the Match Tracker and Analysis API'});
+})
+
+//authentication 
+app.use(authRouter);
+
+app.listen(PORT, function(error){
+    try{
+    console.log(`Server is listening on Port ${PORT}`);
+    }catch(error){
+        console.log(`Server is down. Read the error message here: ${error}`);
+    }
+})
+
